3139c1dd7f2b93e7688ca642395047d1f33a1794
Anilykh <alykseth@icloud.com>
2024-03-19 13:27:06 +0100

    Testing square function

---
+++ Test.cs
@@ -0,0 +1,14 @@
+using System.Globalization;
+
+
+static int Square(int num);
+{
+    int result = 0;
+    for (int i = 0; i < num; i++)
+    {
+        result += num;
+    }
+    return result;
+}
+
+int square = Square(num);
